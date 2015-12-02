e43ddd7c4cb25f984a381092ae1fe890f6b67e06
ExiLio <sandoval.lucasj@gmail.com>
2015-12-01 22:42:02 -0300

    Empezando interfaz grafica

--- src/Consoles/CpuConsole.py
+++ src/Consoles/CpuConsole.py
@@ -1,67 +1,40 @@
 from Tkinter import *
 import codecs
 import os
+from Consoles.ConsoleSecondary import ConsoleSecondary
+from pango import Weight
 
+class CpuConsole(Frame):
 
-v0 = Tk()
-v0.config(bg="black")
-v0.geometry("700x500")
-v1=Toplevel(v0)
+    def __init__(self):
+        self.mainWindow = Tk()
+        self.createContents()
+        
+        
+        #self.windowForCpu = ConsoleSecondary("CONSOLE CPU" , self.mainWindow , "../log/cpu_log")
+        
+    def startUp(self):
+        self.mainWindow.mainloop()
 
-def mostrar(ventana): ventana.deiconify()
-def ocultar(ventana):ventana.withdraw()
-def ejecutar(f): v0.after(200,f)
+    def createContents(self):
+        self.mainWindow.config(bg="black")
+        self.mainWindow.geometry("700x500")
+        text = Text(self.mainWindow)
+        text.pack()
+        text.config(bg="black",fg="white")
+        self.mainWindow.resizable(0,0)
+        self.createButtons()
+        
+    def createButtons(self):
+        cpuButton = Button(self.mainWindow,text="Open Cpu Log")
+        cpuButton.pack()#grid(row=1,column=0)
+        printerButton = Button(self.mainWindow,text="Open Printer Log")
+        printerButton.pack()#.grid(row=1,column=1)
+        otro = Button(self.mainWindow,text="Open askjd Log")
+        otro.pack()#.grid(row=1,column=2)
 
-b1=Button(v0,text="CPU STATE",command=lambda: ejecutar(mostrar(v1)) or imprimir("hola") )
-b1.grid(row=1,column=1)
-b2=Button(v0,text="Hide CPU STATE",command=lambda: ejecutar(ocultar(v1)))
-b2.grid(row=1,column=2)
+if __name__ == '__main__':
 
-def doit(f): v0.after(100, f)
-
-def imprimir(textcontrol): print textcontrol.get('1.0', END+'-1c')
-
- 
-
-def escribir_en_archivo(enlace):
-
-    f = codecs.open(enlace,"w","utf-8")
-
-    texto = t1.get('1.0', END+'-1c')
-
-    f.write(texto)
-
-    f.close()
-
- 
-
-def abrir_archivo(enlace):
-
-    if os.path.exists(enlace):
-
-        f = open(enlace,"r")
-
-        h= f.read()
-
-        t1.insert(END,h)
-
-        f.close()
-
- 
-
-t1=Text(v0)
-
-t1.config(bg="black",fg="white")
-
-t1.pack()
-
- 
-
-b3 = Button(v0,text="<< SAVE >>",command=lambda: doit(escribir_en_archivo('C:\hola.txt')))
-
-b3.config(bg="black",fg="white")
-
-b3.pack()
-
-v1.withdraw()
-v0.mainloop()
+    
+    console = CpuConsole()
+    console.startUp()
